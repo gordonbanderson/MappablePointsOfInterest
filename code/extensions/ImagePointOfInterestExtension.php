585f644f0c67bed77a8f45e06f0c2725f4363ed7
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-12-28 12:45:19 +0700

    FIX: Remove method that does nothing

--- code/extensions/ImagePointOfInterestExtension.php
+++ code/extensions/ImagePointOfInterestExtension.php
@@ -4,9 +4,4 @@ class ImagePointOfInterestExtension extends DataExtension {
 	private static $has_one = array(
 		'POIImage' => 'Image'
 	);
-
-	/* The openstreetmap id is only for scripting purposes */
-    public function updateCMSFields(FieldList $fields) {
-	    //$osmid = $fields->removeByName('OpenStreetMapID');
-	}
 }
