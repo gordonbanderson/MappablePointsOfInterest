bd9ca9bb1dd4df27bbcab30ae94961b99eb1a776
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-12-28 20:59:48 +0700

    MINOR: Whitespace

--- tests/NearestPOIPageTest.php
+++ tests/NearestPOIPageTest.php
@@ -1,22 +1,19 @@
 <?php
 
-	class NearestPOIPageTest extends SapphireTest {
-		public function testCMSFields() {
-			$np = new NearestPOIPage();
-			$fields = $np->getCMSFields();
-			$tab = $fields->findOrMakeTab('Root.Layer');
-			$fields = $tab->FieldList();
-			$names = array();
-			foreach ($fields as $field) {
-				$names[] = $field->getName();
-			}
-			$expected = array('PointsOfInterestLayerID');
-			$this->assertEquals($expected, $names);
+class NearestPOIPageTest extends SapphireTest {
+
+	protected static $fixture_file = 'mappable-poi/tests/pointsofinterest.yml';
+
+	public function testCMSFields() {
+		$np = new NearestPOIPage();
+		$fields = $np->getCMSFields();
+		$tab = $fields->findOrMakeTab('Root.Layer');
+		$fields = $tab->FieldList();
+		$names = array();
+		foreach ($fields as $field) {
+			$names[] = $field->getName();
 		}
-
-
-		public function testFind() {
-			$this->markTestSkipped('TODO');
-		}
-
+		$expected = array('PointsOfInterestLayerID');
+		$this->assertEquals($expected, $names);
+	}
 }
