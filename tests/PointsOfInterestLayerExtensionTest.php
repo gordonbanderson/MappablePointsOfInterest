934b1019e10eaa86c7ad6a1b99f643e53e9c5c98
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-12-28 15:56:28 +0700

    ENHANCEMENT: Test the points of interest layer extension cms fields

--- tests/PointsOfInterestLayerExtensionTest.php
+++ tests/PointsOfInterestLayerExtensionTest.php
@@ -1,8 +1,29 @@
 <?php
 
 	class PointsOfInterestLayerExtensionTest extends SapphireTest {
-		public function testUpdateCMSFields() {
-			$this->markTestSkipped('TODO');
-		}
+		
+	public function setUpOnce() {
+		$this->requiredExtensions = array(
+			'POILayerExtensionPageTO' => array('MapExtension', 'PointsOfInterestLayerExtension')
+		);
+		parent::setUpOnce();
+	}
 
+	public function testUpdateCMSFields() {
+		$page = new POILayerExtensionPageTO();
+		$fields = $page->getCMSFields();
+		$tab = $fields->findOrMakeTab('Root.MapLayers');
+		$fields = $tab->FieldList();
+		$names = array();
+		foreach ($fields as $field) {
+			$names[] = $field->getName();
+		}
+		 
+		$this->assertEquals(array('POI Layers'), $names);
+	}
+
+}
+
+
+class POILayerExtensionPageTO extends Page implements TestOnly {
 }
