2cf49b9ee2f8662615de30ec2e7a3f371dc5eb2e
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-12-28 12:16:31 +0700

    ENHANCEMENT: Test for OSM Point of Interest extension

--- tests/OSMPointOfInterestExtensionTest.php
+++ tests/OSMPointOfInterestExtensionTest.php
@@ -1,8 +1,28 @@
 <?php
 
-	class OSMPointOfInterestExtensionTest extends SapphireTest {
-		public function testUpdateCMSFields() {
-			$this->markTestSkipped('TODO');
+class OSMPointOfInterestExtensionTest extends SapphireTest {
+	public function setUpOnce() {
+		$this->requiredExtensions = array(
+			'OSMTestPageTO' => array('MapExtension', 'OSMPointOfInterestExtension')
+		);
+		parent::setupOnce();
+	}
+
+	public function testUpdateCMSFields() {
+		$page = new OSMTestPageTO();
+		$fields = $page->getCMSFields();
+		$tab = $fields->findOrMakeTab('Root.Main');
+		$fields = $tab->FieldList();
+		$names = array();
+		foreach ($fields as $field) {
+			$names[] = $field->getName();
 		}
+		$this->assertNotContains('OpenStreetMapID', $names);
+	}
+
+}
+
+
+class OSMTestPageTO extends Page implements TestOnly {
 
 }
