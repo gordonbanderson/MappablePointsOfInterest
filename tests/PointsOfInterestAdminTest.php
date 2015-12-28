4870f581d585e680335ce1475b24bdd58fa1745c
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-12-28 16:21:52 +0700

    ENHANCEMENT: Added test confirming models

--- tests/PointsOfInterestAdminTest.php
+++ tests/PointsOfInterestAdminTest.php
@@ -1,7 +1,17 @@
 <?php
 
-	class PointsOfInterestAdminTest extends SapphireTest {
-		public function testGetCMSFields() {
-			$this->markTestSkipped('TODO');
-		}
+class PointsOfInterestAdminTest extends SapphireTest {
+	public function testManagedModels() {
+		$modelAdmin = new PointsOfInterestAdmin();
+		$models = $modelAdmin->getManagedModels();
+		$expected = array(
+			'PointsOfInterestLayer' => array(
+				'title' => 'Points Of Interest Layer'
+			),
+			'PointOfInterest' => array(
+				'title' => 'Point Of Interest'
+			)
+		);
+		$this->assertEquals($expected, $models);
+	}
 }
