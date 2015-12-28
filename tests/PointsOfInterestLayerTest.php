d092d728c464e1b0bb3388246e2bd0f893839c79
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-12-28 15:50:42 +0700

    ENHANCEMENT: Test for points of interest layer

--- tests/PointsOfInterestLayerTest.php
+++ tests/PointsOfInterestLayerTest.php
@@ -2,7 +2,24 @@
 
 class PointsOfInterestLayerTest extends SapphireTest {
 
-	public function testGetCMSFields() {
-		$this->markTestSkipped('TODO');
+	public function testUpdateCMSFields() {
+		$layer = new PointsOfInterestLayer();
+		$fields = $layer->getCMSFields();
+		$root = $fields->fieldByName('Root.Main');
+		$fields = $root->FieldList();
+		$names = array();
+		foreach ($fields as $field) {
+			$names[] = $field->getName();
+		}
+		
+		error_log(print_r($names,1)); 
+		$expected = array('Name', 'ShowGuideMarkers', 'DefaultIcon');
+		$this->assertEquals($expected, $names);
+
 	}
+
+}
+
+
+class PointsOfInterestLayerTestObjectTO extends DataObject implements TestOnly {
 }
