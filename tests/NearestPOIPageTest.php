a945279d0a54d38be2765ebb89624dd53fd836ce
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-12-28 11:16:05 +0700

    ENHANCEMENT: Test for additional fields existing of NearestPoiPage

--- tests/NearestPOIPageTest.php
+++ tests/NearestPOIPageTest.php
@@ -1,9 +1,19 @@
 <?php
 
 	class NearestPOIPageTest extends SapphireTest {
-		public function testGetCMSFields() {
-			$this->markTestSkipped('TODO');
+		public function testCMSFields() {
+			$np = new NearestPOIPage();
+			$fields = $np->getCMSFields();
+			$tab = $fields->findOrMakeTab('Root.Layer');
+			$fields = $tab->FieldList();
+			$names = array();
+			foreach ($fields as $field) {
+				$names[] = $field->getName();
+			}
+			$expected = array('PointsOfInterestLayerID');
+			$this->assertEquals($expected, $names);
 		}
+
 
 		public function testFind() {
 			$this->markTestSkipped('TODO');
