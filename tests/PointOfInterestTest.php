ae696e8dbbd8a3d705974b0c11c7f49beb1c0bdc
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-12-28 11:38:05 +0700

    MINOR: Whitespace

--- tests/PointOfInterestTest.php
+++ tests/PointOfInterestTest.php
@@ -1,20 +1,20 @@
 <?php
 
-	class PointOfInterestTest extends SapphireTest {
-		public function testGetCMSFields() {
-			$this->markTestSkipped('TODO');
-		}
+class PointOfInterestTest extends SapphireTest {
+	public function testGetCMSFields() {
+		$this->markTestSkipped('TODO');
+	}
 
-		public function testPopulateDefaults() {
-			$this->markTestSkipped('TODO');
-		}
+	public function testPopulateDefaults() {
+		$this->markTestSkipped('TODO');
+	}
 
-		public function testUpdateElasticsearchMapping() {
-			$this->markTestSkipped('TODO');
-		}
+	public function testUpdateElasticsearchMapping() {
+		$this->markTestSkipped('TODO');
+	}
 
-		public function testUpdateElasticsearchDocument() {
-			$this->markTestSkipped('TODO');
-		}
+	public function testUpdateElasticsearchDocument() {
+		$this->markTestSkipped('TODO');
+	}
 
 }
