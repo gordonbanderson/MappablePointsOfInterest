fde6df4f5b153a06e86194e0001848727df4b8c6
Gordon Anderson <gordon.b.anderson@gmail.com>
2016-01-05 16:18:37 +0700

    FIX: Use the extension points in MapExtension in order toupdate the hasGeo flag and add markers

--- code/PointsOfInterestLayerExtension.php
+++ code/PointsOfInterestLayerExtension.php
@@ -13,6 +13,16 @@ class PointsOfInterestLayerExtension extends DataExtension
     );
 
     /**
+     * Only set has geo to true if layers exist
+     * @param  boolean &$hasGeo will be set to true if any layers
+     */
+    public function updateHasGeo(&$hasGeo) {
+        if ($this->owner->PointsOfInterestLayers()->count() > 0) {
+            $result = true;
+        }
+    }
+
+    /**
      * Update cms fields - add list of POIs.
      *
      * @param FieldList $fields list of existing fields on the object
@@ -30,5 +40,33 @@ class PointsOfInterestLayerExtension extends DataExtension
             $gridConfig2
         );
         $fields->addFieldToTab('Root.MapLayers', $gridField2);
+    }
+
+    /**
+     * Use extension point to add the markers
+     * @param  MapAPI &$map reference to object representing the map
+     * @param  $autozoom will be altered to true for autozoom, false not to
+     */
+    public function updateBasicMap(&$map, &$autozoom) {
+        if (Object::has_extension($this->owner->ClassName, 'PointsOfInterestLayerExtension')) {
+            foreach($this->owner->PointsOfInterestLayers() as $layer) {
+                $layericon = $layer->DefaultIcon();
+                if ($layericon->ID === 0) {
+                    $layericon = null;
+                }
+                foreach ($layer->PointsOfInterest() as $poi) {
+                    if ($poi->MapPinEdited) {
+                        if ($poi->MapPinIconID == 0 && $layericon) {
+                            $poi->CachedMapPinURL = $layericon->getAbsoluteURL();
+                        }
+                        $map->addMarkerAsObject($poi);
+
+                        // we have a point of interest, so turn on auto zoom
+                        $autozoom = true;
+                    }
+                }
+            }
+            $map->setClusterer(true);
+        }
     }
 }
